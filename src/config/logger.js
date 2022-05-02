742953add3592ebc3449ede66f5aabb435f3545d
m.jamali <m.jamali@tecmasolutions.com>
2022-05-02 23:44:09 +0200

    add winston

---
+++ src/config/logger.js
@@ -0,0 +1,18 @@
+const { createLogger, transports } = require('winston');
+const winston = require('winston');
+
+const myFormat = winston.format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`);
+
+const logger = (namespace) => {
+  createLogger({
+    level: process.env.WINSTON_LOG_LEVEL || 'info',
+    format: winston.format.combine(winston.format.timestamp(), myFormat),
+    transports: [new transports.Console()],
+    exceptionHandlers: [new transports.Console()],
+    rejectionHandlers: [new transports.Console()],
+    exitOnError: false,
+    defaultMeta: { ...(namespace ? { namespace } : {}) },
+  });
+};
+
+module.exports = logger;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const app = require('./config/express');
 const { port } = require('./config/vars');
+const Logger = require('./config/logger');
 
-const server = app.listen(port, '0.0.0.0', () => console.log(`Server started on port ${port}`));
+const logger = Logger(__filename.replace(`${__dirname}`, ''));
+
+// Listen to the requests
+const server = app.listen(port, '0.0.0.0', () => logger.ino(`Server started on port ${port}`));
 
 module.exports = server;
