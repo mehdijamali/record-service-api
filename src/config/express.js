942cb4f32adea7ef11e2ac6e2e35831096dace3f
m.jamali <m.jamali@tecmasolutions.com>
2022-05-03 08:48:20 +0200

    add compression

--- src/config/express.js
+++ src/config/express.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
+const compress = require('compression');
 
 const { ValidationError } = require('express-validation');
 
 const app = express();
+
 app.use(morgan('tiny'));
 
+// gzip compression
+app.use(compress());
+
+// handle validation errors
 app.use((err, req, res, next) => {
   next();
   if (err instanceof ValidationError) {
@@ -16,7 +22,7 @@ app.use((err, req, res, next) => {
 
   return res
     .status(500)
-    .json({ code: err.statusCode, msg: 'Internal Server Error' });
+    .json({ code: 500, msg: 'Internal Server Error' });
 });
 
 module.exports = app;
