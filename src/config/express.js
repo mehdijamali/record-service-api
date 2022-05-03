919f75a6651a4221063d26f699a931029c01e5dd
m.jamali <m.jamali@tecmasolutions.com>
2022-05-03 08:47:12 +0200

    add morgan

--- src/config/express.js
+++ src/config/express.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const morgan = require('morgan');
+
+const { ValidationError } = require('express-validation');
 
 const app = express();
+app.use(morgan('tiny'));
+
+app.use((err, req, res, next) => {
+  next();
+  if (err instanceof ValidationError) {
+    return res
+      .status(err.statusCode)
+      .json({ code: err.statusCode, msg: err.details.body[0].message });
+  }
+
+  return res
+    .status(500)
+    .json({ code: err.statusCode, msg: 'Internal Server Error' });
+});
 
 module.exports = app;
