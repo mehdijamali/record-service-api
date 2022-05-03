03ddbadca2abc02525b105dd3885369d43937e03
m.jamali <m.jamali@tecmasolutions.com>
2022-05-03 08:51:56 +0200

    add cors, json parse and logger in express config

--- src/config/express.js
+++ src/config/express.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const compress = require('compression');
+const cors = require('cors');
+const { ValidationError } = require('express-validation');
 const helmet = require('helmet');
 
-const { ValidationError } = require('express-validation');
+const routes = require('../api/routes/v1');
+const logger = require('./logger')(__filename.replace(`${__dirname}`, ''));
 
 const app = express();
 
@@ -15,6 +18,13 @@ app.use(compress());
 // secure apps by setting various HTTP headers
 app.use(helmet());
 
+// parse json request body
+app.use(express.json());
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
+
+// mount api v1 routes
+app.use('/api/v1', routes);
 
 // handle validation errors
 app.use((err, req, res, next) => {
@@ -25,9 +35,13 @@ app.use((err, req, res, next) => {
       .json({ code: err.statusCode, msg: err.details.body[0].message });
   }
 
-  return res
-    .status(500)
-    .json({ code: 500, msg: 'Internal Server Error' });
+  return res.status(500).json({ code: 500, msg: 'Internal Server Error' });
 });
+
+process.on('unhandledRejection', (error) => {
+  logger.error(error.message);
+});
+
+process.on('uncaughtException', (error) => logger.error(error.message));
 
 module.exports = app;
